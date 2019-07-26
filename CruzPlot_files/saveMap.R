806ea69312d463b76e1b8a389dd317772efcea08
Sam Woodman <swoodman213@gmail.com>
2019-07-26 12:35:25 -0700

    User can specify res of saved map

--- CruzPlot_files/saveMap.R
+++ CruzPlot_files/saveMap.R
@@ -7,29 +7,30 @@ plotDownload <- function() {
 
 output$downloadMap <- downloadHandler(
   filename = function() {
-    file.ext <- switch(input$download.format, 
-                       "1" = ".jpeg", "2" = ".pdf", "3" = ".png")
+    file.ext <- switch(input$download.format, "1" = ".jpeg", "2" = ".pdf", "3" = ".png")
     
-    paste0("cruzPlot_", 
-           cruz.map.range$lon.range[1], "_", 
-           cruz.map.range$lon.range[2], "_", 
-           cruz.map.range$lat.range[1], "_", 
-           cruz.map.range$lat.range[2], 
-           file.ext) 
+    paste0(
+      "cruzPlot_", 
+      cruz.map.range$lon.range[1], "_", cruz.map.range$lon.range[2], "_", 
+      cruz.map.range$lat.range[1], "_", cruz.map.range$lat.range[2], 
+      file.ext
+    ) 
   },
   content = function(file) {
+    file.res <- switch(input$download.res, "1" = 200, "2" = 72)
+    
     if(input$download.format == 1) {
-      jpeg(file, width = 480, height = 480, quality = 150)
+      jpeg(file, width = 10, height = 10, units = "in", res = file.res)
       plotDownload()
       dev.off()
     }
     if(input$download.format == 2) {
-      pdf(file, width = 12, height = 10)
+      pdf(file, width = 10, height = 10)
       plotDownload()
       dev.off()
     }
     if(input$download.format == 3) {
-      png(file, width = 600, height = 480)
+      png(file, width = 10, height = 10, units = "in", res = file.res)
       plotDownload()
       dev.off()
     }
