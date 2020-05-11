9f17127d8e39db7791f21ece7ab3056277bf5d61
Sam Woodman <swoodman213@gmail.com>
2020-05-10 19:11:24 -0700

    cmd check fixes

--- R/cruzplot_gui.R
+++ R/cruzplot_gui.R
@@ -2,20 +2,19 @@
 #'
 #' Open the CruzPlot utility program, an R Shiny application
 #'
-#' @param launch.browser Logical with default of \code{TRUE}; passed to \code{launch.browser}
-#'   argument of \code{\link[shiny]{runApp}}#'
-#' @param ... Passed to \code{\link[shiny]{runApp}}
+#' @param launch.browser Logical with default of \code{TRUE};
+#'   passed to \code{launch.browser} argument of \code{\link[shiny]{runApp}}
 #'
 #' @usage cruzplot_gui(launch.browser = TRUE)
 #'
 #' @seealso # TODOurl
 #'
 #' @export
-cruzplot_gui <- function(launch.browser = TRUE, ...) {
+cruzplot_gui <- function(launch.browser = TRUE) {
   appDir <- system.file("shiny", package = "CruzPlot")
   if (appDir == "") {
     stop("There was an error opening CruzPlot; try re-installing 'CruzPlot'",
          call. = FALSE)
   }
-  runApp(appDir, launch.browser = launch.browser, display.mode = "normal", ...)
+  runApp(appDir, launch.browser = launch.browser, display.mode = "normal")
 }
