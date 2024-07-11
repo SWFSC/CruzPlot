554d1937948e2712bf97b35915b0d9692cee5153
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2024-07-11 16:37:49 -0700

    doc update

--- R/CruzPlot-package.R
+++ R/CruzPlot-package.R
@@ -1,13 +1,13 @@
 #' Create maps of shipboard DAS data
 #'
 #' A utility program oriented to create maps, plot data, and do basic data summaries
-#' from data files in the "DAS" format produced by WinCruz.
+#' from data files in the "DAS" format, typically produced by WinCruz.
 #'
 #' @name CruzPlot-package
 #' @aliases cruzplot CruzPlot
 #' @title CruzPlot
 #' @author Sam Woodman \email{sam.woodman@@noaa.gov}
-#' @seealso \url{https://smwoodman.github.io/CruzPlot/}
+#' @seealso \url{https://swfsc.github.io/CruzPlot/}
 #'
 #' @importFrom shiny runApp
 #'
